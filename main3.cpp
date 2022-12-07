d452fe635045884424d34357dd9105e9577e6d3b
Mubi <mubashirwasi7@gmail.com>
2022-12-08 01:15:12 +0500

    Update main3.cpp

--- main3.cpp
+++ main3.cpp
@@ -1,5 +1,5 @@
-#include<graphics.h>										//used for literally everything, LOL
-#include<time.h>											//used for delay function in loops and totally randomizing the rand function from srand
+#include<graphics.h>							//used for literally everything, LOL
+#include<time.h>								//used for delay function in loops and totally randomizing the rand function from srand
 //stdlib.h not used as it is avaiable in time.h header		//here used for all calculations, int, char and much more
 //conio.h not used as it is available in graphics.h			//here used for getch(),
 //windows.h not used as it is available in graphics.h		//here used for GetAsyncKeyState(),VK_RETURN, VK_LEFT, VK_RIGHT, VK_UP, VK_DOWN
