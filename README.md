b09fd2175d50c19ed72d3cdacd69c7bf8a1ad6eb
Mubi <mubashirwasi7@gmail.com>
2022-12-07 12:09:31 +0500

    updated
    comments added in code
    readme gives a little description of code

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ Instructor:      Syeda Faiza Adnan
 
 Assignment:      Snake Game with added graphics
 
+DESCRIPTION: 
+    1) int x[100], y[100] are arrays of size 100 bits will be used in almost all parts of code from food to snake to movement to respawninig of food
+    2) int rx and ry are the axis on which the food will spawn
+    3) int gm and gd are graphics mode and graphics drivers they are either to be detected via gm, gd = DETECT or detectgraph(&gm,&gd) where &gm and &gd are the memory adresses of the mode and drivers files
+    4) int l will be used in loop for making the size of snake
+    5) int d = 2 will be used in if else conditions for 
--- main3.cpp
+++ main3.cpp
@@ -3,25 +3,25 @@
 
 int main()
 {
-	int i,X[100],Y[100],rx,ry,gm,gd = DETECT,l,d=2,s=16;
+	int i,X[100],Y[100],rx,ry,gm,gd,l,d=2,s=16;
 	srand(time(NULL));
 	detectgraph(&gd,&gm);
 	initgraph(&gd,&gm,NULL);
-	setfillstyle(1,3); //borders code start
+	setfillstyle(1,1); //border code starts
 	bar(s-s/2,s-s/2,(s/2)+s*(1350/s),s+s/2); //top
 	bar(s-s/2,(-s/2)+s*(700/s),(s/2)+s*(1350/s),(s/2)+s*(700/s)); //bottom
 	bar(s-s/2,s-s/2,s+s/2,(s/2)+s*(700/s)); //left
 	bar((-s/2)+s*(1350/s),s-s/2,(s/2)+s*(1350/s),(s/2)+s*(700/s)); //right
 	X[0]=s*(1350/(2*s)); Y[0]=s*(700/(2*s));
-    bar(X[0]-s/2,Y[0]-s/2,X[0]+s/2,Y[0]+s/2);
+    bar(X[0]-s/2,Y[0]-s/2,X[0]+s/2,Y[0]+s/2); //snake bar (1 square)
     l=5;
-    for(i=1;i<l;i++) //to know 
+    for(i=1;i<l;i++) //for enlarging snake in initial state (4 blocks)
     {
         X[i]=X[0]-(i*s);
         Y[i]=Y[0];
         bar(X[i]-s/2,Y[i]-s/2,X[i]+s/2,Y[i]+s/2);
     }
-    rx=s; ry=s; //food starts
+    rx=s; ry=s; //food starts 
     setfillstyle(1,2);
     while(getpixel(rx,ry)!=0)
 	{
@@ -32,15 +32,14 @@ int main()
     delay(2000);
 	while(1)
 	{
-    	setfillstyle(1,0); //updating the snake 
+    	setfillstyle(1,0); //updating the snake
 		bar(X[l-1]-s/2,Y[l-1]-s/2,X[l-1]+s/2,Y[l-1]+s/2);
 		for(i=l-1;i>0;i--)
     	{
     		X[i]=X[i-1];
     		Y[i]=Y[i-1];
 		}
-		// updating the head
-    	if(d==0)
+    	if(d==0) //updating the head
 			X[0]=X[0]-s;
 		if(d==1)
 			Y[0]=Y[0]-s;	
@@ -48,11 +47,9 @@ int main()
 			X[0]=X[0]+s;
 		else if(d==3)
 			Y[0]=Y[0]+s;
-		//terminating condition
-		if(getpixel(X[0],Y[0])==1)
+		if(getpixel(X[0],Y[0])==1) //terminating condition 
 			break;
-		//updating direction
-		if((GetAsyncKeyState(VK_RIGHT))&&(d!=0))
+		if((GetAsyncKeyState(VK_RIGHT))&&(d!=0)) //updating directions i.e. taking input
 			d=2;
 		else if((GetAsyncKeyState(VK_UP))&&(d!=3))
 			d=1;
@@ -60,7 +57,7 @@ int main()
 			d=0;
 		else if((GetAsyncKeyState(VK_DOWN))&&(d!=1))
 			d=3;
-		if(getpixel(X[0],Y[0])==2) //food taken 
+		if(getpixel(X[0],Y[0])==2) //food taken ?
 		{
 			rx=s; ry=s;
 			setfillstyle(1,2);
@@ -72,17 +69,14 @@ int main()
 			bar(rx-s/2,ry-s/2,rx+s/2,ry+s/2);
 			l=l+1;
 		}
-		//displaying the snake
-		setfillstyle(1,1);
+		setfillstyle(1,1); //displaying the snake
 		for(i=0;i<l;i++)
        		bar(X[i]-s/2,Y[i]-s/2,X[i]+s/2,Y[i]+s/2);	
 		delay(100);
     }
-    printf("score : %d",l-5); //printing the snake
-	while(!GetAsyncKeyState(VK_RETURN));
-	closegraph();
-	getch();
-getch();
+    printf("score : %d",l-5); //displaying the score in seperate terminal
+	while(!GetAsyncKeyState(VK_RETURN)); //closing condition i.e. enter key
+	closegraph(); //close the seperate BGI windows displaying the graphcis 
+	getch(); //hold terminal windows so the score is visible
 	return 0;	
 }
-
