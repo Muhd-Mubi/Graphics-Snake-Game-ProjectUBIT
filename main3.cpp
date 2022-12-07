ea80414874e4a37e1f02e2a8931a51e30a484690
Mubi <mubashirwasi7@gmail.com>
2022-12-08 01:10:55 +0500

    update

--- main3.cpp
+++ main3.cpp
@@ -1,9 +1,14 @@
-#include<graphics.h>
-#include<time.h>
+#include<graphics.h>										//used for literally everything, LOL
+#include<time.h>											//used for delay function in loops and totally randomizing the rand function from srand
+//stdlib.h not used as it is avaiable in time.h header		//here used for all calculations, int, char and much more
+//conio.h not used as it is available in graphics.h			//here used for getch(),
+//windows.h not used as it is available in graphics.h		//here used for GetAsyncKeyState(),VK_RETURN, VK_LEFT, VK_RIGHT, VK_UP, VK_DOWN
+//strings.h not used as it is available in graphics.h		//here used for sprintf()
 
 int main()
 {
-	int i,X[100],Y[100],rx,ry,gm,gd,l,d=2,s=16;
+	int i,X[100],Y[100],rx,ry,gm,gd,l,d=1,s=16,highscore=56;
+	char str[1000], str1[1000];//may be removed
 	srand(time(NULL));
 	detectgraph(&gd,&gm);
 	initgraph(&gd,&gm,NULL);
@@ -71,12 +76,16 @@ int main()
 		}
 		setfillstyle(1,1); //displaying the snake
 		for(i=0;i<l;i++)
-       		bar(X[i]-s/2,Y[i]-s/2,X[i]+s/2,Y[i]+s/2);	
-		delay(100);
+			bar(X[i]-s/2,Y[i]-s/2,X[i]+s/2,Y[i]+s/2);	
+			delay(60);
+			setfillstyle(1,2);
+			sprintf(str,"Score: %d",l-5);//changing diffrent data type to string a = string name b = another datatype 
+			settextstyle(4,0,3);//used for setting text size, font and direction (that I dont understand) a = font, b = direction, c = size
+			outtextxy(50,220,str);//give an output prompt on the BGI interface a = x-coordinate, b = y-coordinate, c = the string that is to be printed
     }
     printf("score : %d",l-5); //displaying the score in seperate terminal
 	while(!GetAsyncKeyState(VK_RETURN)); //closing condition i.e. enter key
-	closegraph(); //close the seperate BGI windows displaying the graphcis 
+	closegraph(); //close the seperate BGI windows displaying the graphcis  
 	getch(); //hold terminal windows so the score is visible
 	return 0;	
-}
+}	
